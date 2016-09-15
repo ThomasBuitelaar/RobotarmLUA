1422301925137f6964561f7761e59ecca66a1a5f
Thomas Buitelaar <99041950@mydavinci.nl>
2016-09-15 14:09:46 +0200

    added exercise 10

---
+++ robotarm Thomas/exercise 10.lua
@@ -0,0 +1,17 @@
+require 'robot_arm'
+robot_arm:load_level('exercise 10')
+robot_arm.speed = 0.999
+for _ = 1,6 do
+  robot_arm:grab()
+  robot_arm:scan()
+  print(robot_arm:scan()) 
+  color = robot_arm:scan()
+  if color=='white' then
+    robot_arm:move_right()
+    robot_arm:drop()
+    robot_arm:move_right()
+else
+  robot_arm:drop()
+  robot_arm:move_right()
+end
+end
