dbd11d7a0771e918635d15fb4ddcf399d037995a
Thomas Buitelaar <99041950@mydavinci.nl>
2016-09-26 08:48:06 +0200

    changed speed and changed Exercise 11

--- robotarm Thomas/exercise 1.lua
+++ robotarm Thomas/exercise 1.lua
@@ -1,6 +1,6 @@
 require 'robot_arm'
 robot_arm:load_level('exercise 1')
-robot_arm.speed = 0.999
+robot_arm.speed = 0.800
 
 robot_arm:move_right()
 robot_arm:grab()
--- robotarm Thomas/exercise 11.lua
+++ robotarm Thomas/exercise 11.lua
@@ -1,7 +1,6 @@
 require 'robot_arm'
 robot_arm:load_level('exercise 11')
 robot_arm.speed = 0.999
-
 u=10
 for _ = 1,15 do
 robot_arm:grab()
@@ -14,7 +13,6 @@ end
 robot_arm:drop()
 for _ = 1,u -1 do
   robot_arm:move_left()
-
 end
 else
 u = u -1
